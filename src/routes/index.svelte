5b204082953982671f36620217b5b0c1023074ee
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-07 23:13:57 +0900

    svelte components basic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,21 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    let title = "Ninja Gaming Guides";
+
+    const updateTitle = () =>{
+        title = "something completely title";
+    }
+</script>
+
+<div class="index">
+    <h2>{title}</h2>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet odit cupiditate tenetur optio. Ullam, culpa? Perspiciatis, soluta molestias! Doloremque aliquid ratione beatae hic quasi, similique maxime asperiores fugiat pariatur totam voluptas deserunt repellendus, explicabo officia rem enim fugit incidunt perspiciatis. Omnis nulla nostrum ullam libero nemo? Odio eveniet alias nulla nemo dignissimos quia, pariatur voluptates, asperiores molestias minima porro perspiciatis.</p>
+    <button on:click={updateTitle}>change title</button>
+    <input type="text" bind:value={title}>
+</div>
+<style>
+    .index{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
