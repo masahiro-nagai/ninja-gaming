3b14f935fd8a3fde3f430ba8bbc5bf17613329ea
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-09 23:29:16 +0900

    Pages & Routes

---
+++ src/routes/about.svelte
@@ -0,0 +1,14 @@
+<div class="about">
+    <h2>About Us</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit doloribus maxime officiis ratione fugiat esse sapiente harum blanditiis quod voluptas, recusandae error ad nisi omnis repudiandae aliquid saepe mollitia voluptates cupiditate vel. Esse pariatur deserunt voluptatibus, ipsa soluta culpa cum quaerat placeat porro aperiam nostrum in commodi. Totam unde quae ex omnis reprehenderit, cupiditate, quas dignissimos, error harum sed sapiente.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about{
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,6 +9,8 @@
 <div class="index">
     <h2>{title}</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet odit cupiditate tenetur optio. Ullam, culpa? Perspiciatis, soluta molestias! Doloremque aliquid ratione beatae hic quasi, similique maxime asperiores fugiat pariatur totam voluptas deserunt repellendus, explicabo officia rem enim fugit incidunt perspiciatis. Omnis nulla nostrum ullam libero nemo? Odio eveniet alias nulla nemo dignissimos quia, pariatur voluptates, asperiores molestias minima porro perspiciatis.</p>
+    <a href="/guides">View Guides</a>
+    <a href="/about">About</a>
     <button on:click={updateTitle}>change title</button>
     <input type="text" bind:value={title}>
 </div>
