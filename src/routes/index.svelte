2f817b6a1c72a5965678b9ed41977a504cdd2945
masahiro-nagai <tavernity.so.n.w@gmail.com>
2022-06-09 23:57:03 +0900

    Reusable Components

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,12 @@
 <script>
-    let title = "Ninja Gaming Guides";
+    import Title from "$lib/title.svelte"; 
+    let title = "Welcome";
 
     const updateTitle = () =>{
         title = "something completely title";
     }
 </script>
-
+<Title />
 <div class="index">
     <h2>{title}</h2>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet odit cupiditate tenetur optio. Ullam, culpa? Perspiciatis, soluta molestias! Doloremque aliquid ratione beatae hic quasi, similique maxime asperiores fugiat pariatur totam voluptas deserunt repellendus, explicabo officia rem enim fugit incidunt perspiciatis. Omnis nulla nostrum ullam libero nemo? Odio eveniet alias nulla nemo dignissimos quia, pariatur voluptates, asperiores molestias minima porro perspiciatis.</p>
